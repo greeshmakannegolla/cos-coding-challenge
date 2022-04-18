b1570f7bf2ca767f7331dfa306421c8fe2205b49
Greeshma <greeshma.kannegolla@remotestar.io>
2022-04-18 22:20:38 +0530

    renderflex fixed

--- challenges/flutter/caronsale/lib/screens/user/user_profile_page.dart
+++ challenges/flutter/caronsale/lib/screens/user/user_profile_page.dart
@@ -73,112 +73,114 @@ class _UserProfilePageState extends State<UserProfilePage> {
         backgroundColor: ColorConstants.kAppBackgroundColor,
         body: Padding(
           padding: const EdgeInsets.fromLTRB(18.0, 30, 12, 30),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const CosAppBar(title: "My Profile"),
-              const SizedBox(
-                height: 45,
-              ),
-              Center(
-                child: Column(
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const CosAppBar(title: "My Profile"),
+                const SizedBox(
+                  height: 45,
+                ),
+                Center(
+                  child: Column(
+                    children: [
+                      Avatar(
+                        radius: 60,
+                        url: _profilePicUrl,
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      InkWell(
+                          onTap: _editProfilePhoto,
+                          child: const Text(
+                            'Change profile photo',
+                            style: kUnderlineHeader,
+                          )),
+                      const SizedBox(
+                        height: 50,
+                      ),
+                    ],
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Avatar(
-                      radius: 60,
-                      url: _profilePicUrl,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "EMAIL: ",
+                          style: kHeader.copyWith(fontSize: 16),
+                        ),
+                        const SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          FirebaseAuth.instance.currentUser?.email ?? '',
+                          style: kHeader.copyWith(
+                              fontSize: 20, fontWeight: FontWeight.w400),
+                        )
+                      ],
                     ),
                     const SizedBox(
-                      height: 15,
+                      height: 28,
                     ),
-                    InkWell(
-                        onTap: _editProfilePhoto,
-                        child: const Text(
-                          'Change profile photo',
-                          style: kUnderlineHeader,
-                        )),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "PREFERRED IMAGE UPLOAD MODE: ",
+                          style: kHeader.copyWith(fontSize: 16),
+                        ),
+                        const SizedBox(
+                          height: 6,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Radio(
+                                value: 0,
+                                groupValue: _groupValue,
+                                onChanged: _onRadioButtonChanged),
+                            Text(
+                              'Gallery',
+                              style: kHeader.copyWith(
+                                  fontSize: 20, fontWeight: FontWeight.w400),
+                            ),
+                            Radio(
+                                value: 1,
+                                groupValue: _groupValue,
+                                onChanged: _onRadioButtonChanged),
+                            Text(
+                              'Camera',
+                              style: kHeader.copyWith(
+                                  fontSize: 20, fontWeight: FontWeight.w400),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                     const SizedBox(
-                      height: 50,
+                      height: 70,
                     ),
+                    CosTextButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => ChangePasswordPage()),
+                          );
+                        },
+                        title: "Change Password"),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    CosTextButton(onPressed: _onLogoutPressed, title: "Logout"),
                   ],
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "EMAIL: ",
-                        style: kHeader.copyWith(fontSize: 16),
-                      ),
-                      const SizedBox(
-                        height: 8,
-                      ),
-                      Text(
-                        FirebaseAuth.instance.currentUser?.email ?? '',
-                        style: kHeader.copyWith(
-                            fontSize: 20, fontWeight: FontWeight.w400),
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 28,
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "PREFERRED IMAGE UPLOAD MODE: ",
-                        style: kHeader.copyWith(fontSize: 16),
-                      ),
-                      const SizedBox(
-                        height: 6,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Radio(
-                              value: 0,
-                              groupValue: _groupValue,
-                              onChanged: _onRadioButtonChanged),
-                          Text(
-                            'Gallery',
-                            style: kHeader.copyWith(
-                                fontSize: 20, fontWeight: FontWeight.w400),
-                          ),
-                          Radio(
-                              value: 1,
-                              groupValue: _groupValue,
-                              onChanged: _onRadioButtonChanged),
-                          Text(
-                            'Camera',
-                            style: kHeader.copyWith(
-                                fontSize: 20, fontWeight: FontWeight.w400),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 70,
-                  ),
-                  CosTextButton(
-                      onPressed: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => ChangePasswordPage()),
-                        );
-                      },
-                      title: "Change Password"),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  CosTextButton(onPressed: _onLogoutPressed, title: "Logout"),
-                ],
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
